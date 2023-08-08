9a41f954fc46231648da6a21ae24f73987d7cfaf
Roger Rudin <roger.rudin@mobi.ch>
2023-08-08 17:31:45 +0200

    feat: blog-form

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,8 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+
+type BlogFormGroup = FormGroup<{
+  title: FormControl<string | null>;
+  content: FormControl<string | null>;
+}>;
 
 @Component({
   selector: 'app-add-blog-page',
   templateUrl: './add-blog-page.component.html',
   styleUrls: ['./add-blog-page.component.scss'],
 })
-export class AddBlogPageComponent {}
+export class AddBlogPageComponent implements OnInit {
+  form!: BlogFormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl<string | null>(
+        'an existing title',
+        [
+          Validators.required,
+          Validators.pattern('^[A-Z]+(.)*'),
+          this.customValidator,
+        ],
+        this.customAsyncValidator
+      ),
+      content: new FormControl<string | null>(null, [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    this.validateAllFormFields(this.form);
+
+    if (this.form.valid) {
+      // add Blog
+    }
+  }
+
+  customValidator(
+    control: FormControl<string | null>
+  ): ValidationErrors | null {
+    if (control.value === 'Test') {
+      return { custom: true };
+    }
+    return null;
+  }
+
+  customAsyncValidator(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (control.value === 'Test Async') {
+          resolve({ customAsync: true });
+        }
+        resolve(null);
+      });
+    });
+  }
+
+  validateAllFormFields(formGroup: BlogFormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+}
--- src/app/features/add-blog-page/add-blog-page.module.ts
+++ src/app/features/add-blog-page/add-blog-page.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddBlogPageComponent } from './add-blog-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Routes = [
   {
@@ -12,6 +16,14 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AddBlogPageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AddBlogPageModule {}
